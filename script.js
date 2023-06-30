cf0b7234c9dd73fc02b458cdff01639e3617ced1
kaviArasu15 <134060086+kaviArasu15@users.noreply.github.com>
2023-06-30 08:05:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Destructuring types
+//1)object
+// const person={
+//     name:"kavi",
+//     age:22
+// }
+// const {name,age}=person;
+// console.log(name);
+
+// 2)Array
+let Games=["cricket","ceram"]
+let [game1,game2]=Games
+console.log(game1);
